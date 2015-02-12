eec7877a75cc1a2103ea5a88dd5d0c3f72ec1aed
Certde <casey.chesshir@me.com>
2015-02-11 18:14:13 -0800

    The most basic file that calls SDL and compiles it.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+/*
+ * Lesson 0: Test to make sure SDL is setup properly
+ */
+int main(int, char**){
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Quit();
+    std::cout << "It worked." << std::endl;
+    return 0;
+}
