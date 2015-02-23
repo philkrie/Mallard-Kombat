d2e2fe5ec36917f8502569ec460596680fa8b8e7
Certde <casey.chesshir@me.com>
2015-02-22 23:36:02 -0800

    apparently the main header file wasn't committed?

---
+++ main.hpp
@@ -0,0 +1,10 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <SDL2/SDL.h>                       // SDL library
+#include <SDL2_ttf/SDL2_ttf.h>                   // SDL font library
+#include <SDL2_mixer/SDL2_mixer.h>                 // SDL sound library
+#include <iostream>
+
+
+#endif
